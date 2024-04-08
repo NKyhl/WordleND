2cb36f0bcaa299e620288c2a6be819b69b465743
Nolan Kyhl <nolan@kyhl.com>
2024-04-08 13:09:02 -0400

    set up db model and connect url paths

--- webapp/wordleND/models.py
+++ webapp/wordleND/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    '''Represents a player profile'''
+    username = models.CharField(max_length=50, primary_key=True)
+    email = models.CharField(max_length=260, unique=True)
+    password = models.CharField(max_length=200)
+    name = models.CharField(max_length=100) # Optional
+
+class Play(models.Model):
+    '''Represents one play of the game and its stats'''
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game_date = models.DateTimeField('date played')
+    outcome = models.BooleanField(default=False)
+    attempts = models.IntegerField(default=0)
