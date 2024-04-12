221f5fe7b31d8e5887c5e3a6fceeab57e9fbb4e0
Nolan Kyhl <nolan@kyhl.com>
2024-04-12 12:22:07 -0400

    Link Profile model to auth.User model

--- webapp/wordleND/models.py
+++ webapp/wordleND/models.py
@@ -1,16 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    '''Represents a player profile'''
-    username = models.CharField(max_length=50, primary_key=True)
-    email = models.CharField(max_length=260, unique=True)
-    password = models.CharField(max_length=200)
+class Profile(models.Model):
+    '''Represents a user's non-auth-related data'''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100, blank=True, null=True, default=None) # Optional
+
 
 class Play(models.Model):
     '''Represents one play of the game and its stats'''
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
     game_date = models.DateTimeField('date played')
     outcome = models.BooleanField(default=False)
     attempts = models.IntegerField(default=0)
