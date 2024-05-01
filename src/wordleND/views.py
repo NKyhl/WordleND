ff8ab7e373974073bbbcbf9c99e85524cfa9f8e7
Nolan Kyhl <nolan@kyhl.com>
2024-05-01 11:12:25 -0400

    save and load game state when in progress

--- src/wordleND/views.py
+++ src/wordleND/views.py
@@ -22,20 +22,79 @@ def home(request):
     if request.user.is_authenticated:
         user_email = request.user.email
 
-    user_balance = view_balance_for_user(access_token, user_email)
+        user_balance = view_balance_for_user(access_token, user_email)
 
-    plays_today = Play.objects.filter(
-        user=user, 
-        in_progress = False,
-        game_date__date=current_date
-    )
+        active_plays_today = Play.objects.filter(
+            user=user, 
+            in_progress = True,
+            game_date__date=current_date
+        ).count()
+
+        completed_plays_today = Play.objects.filter(
+            user=user, 
+            in_progress = False,
+            game_date__date=current_date
+        ).count()
+        
+        print('In Progress Games Today:', active_plays_today)
+        print('Completed Games Today:', completed_plays_today)
+
+        return render(request, "index.html", {
+            'balance': user_balance['amount'],
+            'in_progress': True if active_plays_today else False,
+            'limit_reached': True if completed_plays_today >= 3 else False
+        })
     
-    print(plays_today)
-    
-    return render(request, "index.html", {'balance':user_balance['amount']})
+    return render(request, "index.html", {'balance':0})
 
 def play(request):
-    return render(request, "play.html", {})
+    if not request.user.is_authenticated:
+        return render(request, "play.html")
+
+    # Get current game
+    play = Play.objects.filter(
+        user=request.user,
+        in_progress=True,
+        game_date__date=datetime.now().today()
+    )
+
+    if not play:
+        return redirect('home')
+    else:
+        play = play[0]
+
+    # Get game state
+    print(play)
+    g = GameState.objects.filter(
+        play=play
+    )
+    print(g)
+
+    if not g:
+        return redirect('home')
+    else:
+        g = g[0]
+
+    # Get colors too
+    attempts = [g.attempt1, g.attempt2, g.attempt3, g.attempt4, g.attempt5, g.attempt6]
+    colors = []
+    for attempt in attempts:
+        result = []
+        for i, char in enumerate(attempt):
+            if char == play.word[i].upper():    # Correct
+                result.append('C')
+            elif char in play.word.upper():     # Good
+                result.append('G')
+            else:                               # Bad
+                result.append('B')
+        colors.append(result)
+
+
+    return render(request, "play.html", {
+        'attempts': [g.attempt1, g.attempt2, g.attempt3, g.attempt4, g.attempt5, g.attempt6],
+        'colors': colors,
+        'word': play.word
+    })
 
 def signup(request):
     if request.method == "POST":
@@ -111,23 +170,21 @@ def check_word(request):
         )
 
         if not play:
-            return {
-                'error': 'No Game in Progress'
-            }
+            return redirect('/play')
         else:
             play = play[0]
 
         result = []
         j = json.loads(request.body.decode('utf-8'))
-        word = j['word'].upper()
+        guess = j['guess']
         attempt = j['attempt']
 
         # Validate word
         language_path = f"wordleND/languages/{play.language}.txt"
         with open(language_path, "r") as file:
             words = map(str.upper, map(str.strip, file.readlines()))
-            if word.upper() not in words:
-                print(f'{word} not in words')
+            if guess not in words:
+                print(f'{guess} not in words')
                 result = json.dumps({
                     'valid': False
                 })
@@ -135,30 +192,30 @@ def check_word(request):
 
         # Check word
         correct = True
-        for i, char in enumerate(word):
-            if char == play.word[i]:    # Correct
+        for i, char in enumerate(guess):
+            if char == play.word[i].upper():    # Correct
                 result.append('C')
-            elif char in play.word:     # Good
+            elif char in play.word.upper():     # Good
                 result.append('G')
                 correct = False
-            else:                       # Bad
+            else:                               # Bad
                 result.append('B')
                 correct = False
 
         # Upload game state - TODO
         g = GameState.objects.filter(play=play)[0]
         if attempt == 1:
-            g.attempt1 = word
+            g.attempt1 = guess
         elif attempt == 2:
-            g.attempt2 = word
+            g.attempt2 = guess
         elif attempt == 3:
-            g.attempt3 = word
+            g.attempt3 = guess
         elif attempt == 4:
-            g.attempt4 = word
+            g.attempt4 = guess
         elif attempt == 5:
-            g.attempt5 = word
+            g.attempt5 = guess
         elif attempt == 6:
-            g.attempt6 = word   
+            g.attempt6 = guess   
         g.save() 
 
         # If the game is correct
@@ -180,7 +237,8 @@ def check_word(request):
         result = json.dumps({
             'valid': True,
             'result': result,
-            'correct': correct
+            'correct': correct,
+            'word': play.word
         })
         return HttpResponse(result, content_type='application/json')
 
